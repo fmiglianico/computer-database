fb9ad8d1242f5369076e63ad4917db420a79cbcd
Francois Miglianico <fmiglianico@excilys.com>
2014-01-06 10:45:22 +0100

    Ajout de BoneCP Connection Pool

--- src/com/formation/computerdb/dao/DAOFactory.java
+++ src/com/formation/computerdb/dao/DAOFactory.java
@@ -1,8 +1,13 @@
 package com.formation.computerdb.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Properties;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jolbox.bonecp.BoneCP;
+import com.jolbox.bonecp.BoneCPConfig;
 
 /**
  * DAO Factory
@@ -15,13 +20,17 @@ public enum DAOFactory {
 	 * The instance (Singleton)
 	 */
 	INSTANCE;
+	
+	private static final Logger log = LoggerFactory.getLogger(DAOFactory.class);
+	
+	private BoneCP connectionPool = null;
 
 	//Static final attributes
+	private static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/";
 	private static final String DB_NAME = "computer-database-db";
 	private static final String DB_USER = "root";
 	private static final String DB_PASSWORD = "";
-	
-	private Properties connectionProps = null;
+	private final static int MAX_CONN_PER_PARTITION = 5;
 	
 	// The DAOs
 	private ComputerDAO computerDAO;
@@ -31,16 +40,26 @@ public enum DAOFactory {
 	 * Initializes the connection and the DAOs
 	 */
 	private DAOFactory() {
-		connectionProps = new Properties();
-		connectionProps.put("user", DB_USER);
-		connectionProps.put("password", DB_PASSWORD);
-		connectionProps.put("zeroDateTimeBehavior", "convertToNull");
+		
+		BoneCPConfig config = new BoneCPConfig();
+		config.setJdbcUrl(JDBC_URL + DB_NAME);
+		config.setUsername(DB_USER);
+		config.setPassword(DB_PASSWORD);
+		config.setMaxConnectionsPerPartition(MAX_CONN_PER_PARTITION);
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (Exception e) {
-			System.err.println("Error in DBHandler constructor: " + e.getMessage());
+			System.err.println("Error while loading JDBC driver : " + e.getMessage());
 			e.printStackTrace();
+		}
+		
+		try {
+			connectionPool = new BoneCP(config);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			System.err.println("Error while init of connection pool : " + e1.getMessage());
+			e1.printStackTrace();
 		}
 		
 		computerDAO = new ComputerDAOImpl();
@@ -70,12 +89,13 @@ public enum DAOFactory {
 	public Connection getConn() {
 		Connection conn = null;
 		try {
-			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/"
-					+ DB_NAME, connectionProps);
-		} catch (Exception e) {
-			System.err.println("Error in getConn: " + e.getMessage());
-			e.printStackTrace();
+			conn = connectionPool.getConnection();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			log.error("Error while trying to fetch a connection : " + e1.getMessage());
+			e1.printStackTrace();
 		}
+		log.debug("getConn()");
 		return conn;
 	}
 }
